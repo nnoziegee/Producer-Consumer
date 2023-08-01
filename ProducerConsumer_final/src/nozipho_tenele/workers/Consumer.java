a46524e55707e0f6aca45e819d6ce5ef7a2642b6
Tenele Dlamini <140271603+nsika1102@users.noreply.github.com>
2023-08-01 20:29:58 +0200

    Add files via upload

---
+++ ProducerConsumer_final/src/nozipho_tenele/workers/Consumer.java
@@ -0,0 +1,23 @@
+package nozipho_tenele.workers;
+
+import nozipho_tenele.resource.MyQueue;
+
+public class Consumer implements Runnable {
+	private MyQueue myQueue;
+
+	public Consumer(MyQueue myQueue) {
+		this.myQueue = myQueue;
+	}
+
+	public void run() {
+		while (true) {
+			// consumer get items
+			myQueue.get();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ ProducerConsumer_final/src/nozipho_tenele/workers/Producer.java
@@ -0,0 +1,27 @@
+package nozipho_tenele.workers;
+
+import java.util.Random;
+
+import nozipho_tenele.resource.MyQueue;
+
+public class Producer implements Runnable {
+	private MyQueue myQueue;
+
+	public Producer(MyQueue myQueue) {
+		this.myQueue = myQueue;
+	}
+
+	public void run() {
+		while (true) {
+			Random random = new Random();
+			int data = random.nextInt(100);
+			// producer put items
+			myQueue.put(data);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
